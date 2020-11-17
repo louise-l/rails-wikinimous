d8a3d50e86a220f869c93ba53b2e38ed6add7294
louise longle <louise.longle@gmail.com>
2020-11-17 17:18:55 +0100

    the app is ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+p'creating articles'
+10.times do
+  Article.create(title: Faker::Artist.name, content: Faker::Movies::HarryPotter.quote)
+end
